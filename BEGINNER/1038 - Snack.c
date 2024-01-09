2c9731ccdd5dc4466a6094b97f3cee7a17786323
Vinicius Lawliet <127983906+ViniciusLawliet@users.noreply.github.com>
2024-01-09 17:50:28 -0300

    Added Problem 1038

---
+++ BEGINNER/1038 - Snack.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+struct snack {
+    char specification[16];
+    float price;
+};
+
+int main() {
+
+    struct snack list[5] = {
+        {"Cachorro Quente", 4.00},
+        {"X-Salada", 4.50},
+        {"X-Bacon", 5.00},
+        {"Torrada simples", 2.00},
+        {"Refrigerante", 1.50}
+    };
+
+    int code, quantity;
+    scanf("%d %d", &code, &quantity);
+    printf("Total: R$ %0.2f\n", list[code-1].price * quantity);
+
+    return 0;
+}
