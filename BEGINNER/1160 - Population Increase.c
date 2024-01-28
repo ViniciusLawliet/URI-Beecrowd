27fa8dffdd9a10d40a4d4e4157daba71dc6f85ef
Vinicius Lawliet <127983906+ViniciusLawliet@users.noreply.github.com>
2024-01-28 14:51:06 -0300

    Added Problem 1160

---
+++ BEGINNER/1160 - Population Increase.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+
+    int t, c;
+    scanf("%d", &t);
+
+    long int pa, pb;
+    double g1, g2;
+
+    while (t) {
+
+        t--;
+
+        scanf("%li %li %lf %lf", &pa, &pb, &g1, &g2);
+
+        c = 0;
+        while (pa <= pb && c < 101) {
+            pa += ((pa * g1) / 100);
+            pb += ((pb * g2) / 100);
+
+            c++;
+        }
+
+        (c > 100) ? printf("Mais de 1 seculo.\n") : printf("%d anos.\n", c);
+
+    }
+    
+
+    return 0;
+}
