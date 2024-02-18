77ecb9510a740cb6a53573a0526968cdcff727f2
Vinicius Lawliet <127983906+ViniciusLawliet@users.noreply.github.com>
2024-02-18 19:11:07 -0300

    Added Problem 1961

---
+++ BEGINNER/1961 - Jumping Frog.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int abs_local(int x) {
+    return (x >= 0) ? x : x * -1;
+}
+
+int main() {
+
+    int p, n;
+    scanf("%d %d", &p, &n);
+
+    int v[n], i;
+    for (i = 0; i < n; i++) {
+        scanf("%d", &v[i]);
+    }
+
+    for (i = 1; i < n; i++) {
+
+        if (abs_local(v[i] - v[i - 1]) > p)
+            break;
+
+    }
+    
+    (i == n) ? printf("YOU WIN\n") : printf("GAME OVER\n");
+
+    return 0;
+
+}
