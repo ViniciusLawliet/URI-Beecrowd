d02fb21ebce4bbde3a110c2bbd332227483c3f17
Vinicius Lawliet <127983906+ViniciusLawliet@users.noreply.github.com>
2024-02-28 17:17:55 -0300

    Added Problem 2140

---
+++ BEGINNER/2140.c
@@ -0,0 +1,37 @@
+/**
+ * @author vinicius lawliet
+ * @brief 
+ * @version 0.1
+ * 
+ * problem: 2140 - Two Bills
+ * 
+ */
+
+#include <stdio.h>
+
+int main () {
+
+    int n, m, bills[] = {100, 50, 20, 10, 5, 2};
+
+    while (1) {
+        scanf("%d %d", &n, &m);
+
+        if (!n && !m)
+            break;
+
+        m -= n;
+
+        int i, c;
+        for (i = c = 0; m && i < 6; i++) {
+            if (m / bills[i]) {
+                m -= bills[i];
+                c++;
+            }
+        }
+
+        (!m && c > 1) ? printf("possible\n") : printf("impossible\n");
+
+    }
+
+    return 0;
+}
