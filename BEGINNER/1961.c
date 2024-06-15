cebd4695b47a221e3d282f211c918b0fee12384c
Vinicius Lawliet <127983906+ViniciusLawliet@users.noreply.github.com>
2024-06-15 10:32:45 -0300

    Refactored Memory Allocation

--- BEGINNER/1961.c
+++ BEGINNER/1961.c
@@ -1,13 +1,14 @@
 /**
  * @author vinicius lawliet
  * @brief 
- * @version 0.1
+ * @version 0.2
  * 
  * problem: 1961 - Jumping Frog
  * 
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int abs_local(int x) {
     return (x >= 0) ? x : x * -1;
@@ -18,7 +19,8 @@ int main() {
     int p, n;
     scanf("%d %d", &p, &n);
 
-    int v[n], i;
+    int i, *v;
+    v = (int *)malloc(n * sizeof(int));
     for (i = 0; i < n; i++) {
         scanf("%d", &v[i]);
     }
@@ -31,7 +33,8 @@ int main() {
     }
     
     (i == n) ? printf("YOU WIN\n") : printf("GAME OVER\n");
-
+    free(v);
+    
     return 0;
 
 }
--- BEGINNER/1973.c
+++ BEGINNER/1973.c
@@ -1,20 +1,22 @@
 /**
  * @author vinicius lawliet
  * @brief 
- * @version 0.1
+ * @version 0.2
  * 
  * problem: 1973 - Star Trek
  * 
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main () {
 
     int n;
     scanf("%d", &n);
 
-    int v[n], i;
+    int i, *v;
+    v = (int *)malloc(n * sizeof(int));
     long long unsigned s = 0;
     for (i = 0; i < n; i++) {
 
@@ -40,6 +42,7 @@ int main () {
     }
 
     printf("%d %llu\n", ++c, s);
+    free(v);
 
     return 0;
 
